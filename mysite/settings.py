767960fd96a32a6a919c5d504dcea637ffc090f7
Hansen-Susilo <143732737+Hansen-Susilo@users.noreply.github.com>
2024-02-21 00:40:00 +0700

    sixteenth changes for vercel deployment

--- mysite/settings.py
+++ mysite/settings.py
@@ -114,6 +114,8 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
